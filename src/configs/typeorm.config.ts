f3f4415d0e9cf50f6625e94946607693af97844e
변민우 <yja05092@gmail.com>
2024-04-25 00:38:08 +0900

    도커 설정

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -10,5 +10,5 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
-  synchronize: false,
+  synchronize: true,
 };
